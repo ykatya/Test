dae6beee94ab76c7e4fba8ad6dc84972f6b03ae4
ykatya <eekat19@gmail.com>
2022-07-11 13:53:13 +0300

    commit2

--- src/mypackage/Main.java
+++ src/mypackage/Main.java
@@ -2,6 +2,6 @@ package mypackage;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello new world 2!");
+        System.out.println("Hello new world 2222!");
     }
 }
